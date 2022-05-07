e692c8f38b512402bb3ab5fc7714ef00400855d8
dakkai <franco.adriel.garcia@gmail.com>
2022-05-07 15:05:09 -0300

    push para deploy

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ conn.sync({ force: true }).then(async () => {
           nombre: el
       }
   }))
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
